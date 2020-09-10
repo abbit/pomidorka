4df7cda8ead326b541172f1053175caedec32e74
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2020-09-10 21:48:48 +0700

    add deploy script

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,19 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 const commonConfig = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+const isAnalyze = process.env.BUNDLE_ANALYZE === true;
+
+const plugins = [
+	new WorkboxPlugin.InjectManifest({
+		swSrc: './src/service_worker.ts',
+		swDest: 'sw.js',
+	}),
+];
+
+if (isAnalyze) {
+	plugins.push(new BundleAnalyzerPlugin());
+}
+
 module.exports = merge(commonConfig, {
 	mode: 'production',
 	devtool: '',
@@ -19,11 +32,5 @@ module.exports = merge(commonConfig, {
 			},
 		},
 	},
-	plugins: [
-		new WorkboxPlugin.InjectManifest({
-			swSrc: './src/service_worker.ts',
-			swDest: 'sw.js',
-		}),
-		new BundleAnalyzerPlugin(),
-	],
+	plugins,
 });
